d8403ce6d2de3817938bc2f783a4e36c7ccc9d9e
DJRoche509 <davjroche@gmail.com>
2023-06-22 23:31:49 -0500

    Created private fields, Constructors added, & Getters Setters

--- src/main/java/com/devmountain/noteApp/entities/Note.java
+++ src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -1,4 +1,38 @@
 package com.devmountain.noteApp.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Notes")
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String body;
+
+    public Note() {
+    }
+
+    public Note(Long id, String body) {
+        this.id = id;
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
