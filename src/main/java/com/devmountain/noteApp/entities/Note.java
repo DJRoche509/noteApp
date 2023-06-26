578a039930349e3d0782954634ec648fa237fa64
DJRoche509 <davjroche@gmail.com>
2023-06-26 12:17:29 -0500

    Update with @Data @AllArgsConstructor annotations

--- src/main/java/com/devmountain/noteApp/entities/Note.java
+++ src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -1,14 +1,19 @@
 package com.devmountain.noteApp.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.devmountain.noteApp.entities.User;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Notes")
-/*  The following 3 lines simplifies the class without the need of creating Constructors, getters, & setters
+//  The following 3 lines simplifies the class without the need of creating Constructors, getters, & setters
   @Data
   @AllArgsConstructor
   @NoArgsConstructor
- */
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,27 +22,28 @@ public class Note {
     @Column(columnDefinition = "text")
     private String body;
 
-    public Note() {
-    }
+    @ManyToOne
+    @JsonBackReference
+    private User user;
 
-    public Note(Long id, String body) {
-        this.id = id;
-        this.body = body;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
+//    public Note(Long id, String body) {
+//        this.id = id;
+//        this.body = body;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getBody() {
+//        return body;
+//    }
+//
+//    public void setBody(String body) {
+//        this.body = body;
+//    }
 }
