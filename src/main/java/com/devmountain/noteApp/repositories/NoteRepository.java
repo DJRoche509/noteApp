7201e2ce90ab6695f64dc012a25d691478c89af0
DJRoche509 <davjroche@gmail.com>
2023-06-27 13:54:58 -0500

    Added List<note> findAllByUserEquals()

--- src/main/java/com/devmountain/noteApp/repositories/NoteRepository.java
+++ src/main/java/com/devmountain/noteApp/repositories/NoteRepository.java
@@ -5,6 +5,9 @@ import com.devmountain.noteApp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
+public interface NoteRepository extends JpaRepository <Note, Long> {
+    List<Note> findAllByUserEquals(User user);
 }
